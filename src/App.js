08375a6cb44c7a8c8000e27545a093e69c895398
chathura jayaranga <cj.chathura.jayaranga@gmail.com>
2025-03-20 12:47:35 +0530

    init components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Fragment } from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Header>
+      <Nav />
+    </Header>
+    <Main></Main>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../assets/Logo.svg';
+
+function Nav() {
+  return (
+    <nav>
+        <ul>
+            <li><a href='#home'><img src={logo} alt="Little Lemon" /> </a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#blog">Blog</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
